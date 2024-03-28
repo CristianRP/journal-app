2e2c47ce5272e2b0eac4ff9aa7aaac989b9cb4cf
Cristian Ramirez <cristianramirezgt@gmail.com>
2024-03-28 02:12:45 -0600

    feat: Add NavBar, SideBar and update JournalLayout and JournalPage components

---
+++ src/journal/components/SideBar.tsx
@@ -0,0 +1,48 @@
+import { TurnedInNot } from '@mui/icons-material';
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
+
+interface SideBarProps {
+  drawerWidth: number;
+}
+
+export const SideBar = ({ drawerWidth }: SideBarProps) => {
+  return (
+    <Box
+      component='nav'
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant='permanent'
+        open
+        sx={{
+          display: { xs: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+        }}
+      >
+        <Toolbar>
+          <Typography variant='h6' noWrap component='div'>
+            Cristian Ramírez
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {
+            ['January', 'February', 'March', 'April'].map( text => (
+              <ListItem key={ text } disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <TurnedInNot />
+                  </ListItemIcon>
+                  <Grid container>
+                    <ListItemText primary={ text } />
+                    <ListItemText secondary={ 'Deserunt occaecat esse cillum proident commodo eu.' } />
+                  </Grid>
+                </ListItemButton>
+              </ListItem>
+            ))
+          }
+        </List>
+      </Drawer>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,11 +1,12 @@
-import { MailOutline } from '@mui/icons-material'
 import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+      <Typography>
+        Sint adipisicing labore pariatur veniam. Duis sit nostrud laboris est esse elit voluptate commodo ullamco est sint sit elit. Lorem est anim occaecat ullamco mollit laborum aliquip non officia aliqua excepteur dolor. Sint excepteur esse exercitation nulla laborum ad ut ullamco sit qui aliquip anim eiusmod. Commodo consectetur proident esse pariatur.
+      </Typography>
+    </JournalLayout>
   )
 }
