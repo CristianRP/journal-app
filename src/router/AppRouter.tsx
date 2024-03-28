4a2c2cd89fa8c6692c58dea659e4d95c4ecbccc2
Cristian Ramirez <cristianramirezgt@gmail.com>
2024-03-28 00:15:46 -0600

    feat: Add logic to config routes for Login, Register and Journal

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
+import { AuthRoutes } from '../auth/routes/AuthRoutes'
+import { JournalRoutes } from '../journal/routes/JournalRoutes'
 
 export const AppRouter = () => {
   return (
     <Routes>
       {/* Login and Register */}
-      <Route />
+      <Route path='/auth/*' element={ <AuthRoutes /> } />
 
       {/* JournalApp */}
-      <Route />
+      <Route path='/*' element={ <JournalRoutes /> } />
     </Routes>
   )
 }
