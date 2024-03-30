83955c45a160b137435665dfdc55d820c233f7ca
Cristian Ramirez <cristianramirezgt@gmail.com>
2024-03-29 19:06:47 -0600

    feat: Update useForm custom hook to accept validations for fields and add types

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,7 +1,17 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
 
 type FormState<T> = {
   [K in keyof T]: string;
+}
+
+type ValidationFunction = (value: string) => boolean;
+
+export type FormValidations<T> = {
+  [K in keyof T]: [ValidationFunction, string];
+}
+
+type ValidationResult<T> = {
+  [K in keyof T as `${Extract<K, string>}Valid`]: string;
 }
 
 type UseForm<T> = {
@@ -10,10 +20,42 @@ type UseForm<T> = {
   formState: FormState<T>;
   onInputChange: (event: ChangeEvent<HTMLInputElement>) => void;
   onResetForm: () => void;
+} & ValidationResult<T>
+& {
+  isFormValid: boolean;
 }
 
-export const useForm = <T extends object>( initialForm: T = {} as T ): UseForm<T> => {
+export const useForm = <T>( initialForm: T = {} as T, formValidations: FormValidations<T> = {} as FormValidations<T>): UseForm<T> => {
+
   const [ formState, setFormState ] = useState<FormState<T>>( initialForm as FormState<T> );
+  const [ formValidation, setFormValidation ] = useState<ValidationResult<T>>({} as ValidationResult<T>);
+
+  useEffect(() => {
+    console.log('use-effect');
+    
+    const createValidators = () => {
+      const formCheckedValues: Record<string, string> = {} as ValidationResult<T>;
+  
+      for (const formField of Object.keys( formValidations )) {
+        const [ fn, errorMessage = 'Field is required' ] = formValidations[formField as keyof T];
+        const fieldValue = formState[formField as keyof T];
+        
+        formCheckedValues[`${ formField }Valid`] = fn( fieldValue ) ? '' : errorMessage;
+      }      
+      setFormValidation( formCheckedValues as ValidationResult<T> );
+    }
+    
+    createValidators();
+  }, [ formState, formValidations ]);
+
+  const isFormValid = useMemo( () => {
+    for (const formValue of Object.keys(formValidation)) {
+      if (formValidation[formValue as keyof typeof formValidation] !== null) return false;
+    }
+
+    return true;
+  }, [ formValidation ])
+  
 
   const onInputChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = target;
@@ -32,5 +74,7 @@ export const useForm = <T extends object>( initialForm: T = {} as T ): UseForm<T
     formState,
     onInputChange,
     onResetForm,
+    ...formValidation,
+    isFormValid,
   }
 }
